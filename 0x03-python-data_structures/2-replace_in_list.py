76b5f1d7f8697589247adbdd504a09951a514f7c
Bn-Bushra <aybuhabi@gmail.com>
2023-03-03 16:15:29 +0100

    inventing a new method

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,8 +5,8 @@ def replace_in_list(my_list, idx, element):
     else:
         my_list.remove(my_list[idx])
         my_list.insert(idx, element)
-        return my_list[idx]
-    
+        return my_list
+
 # cube = [1, 3, 5, 9, 11, 13]
 # cube.remove(cube[1])
 # cube.insert(1,8)
