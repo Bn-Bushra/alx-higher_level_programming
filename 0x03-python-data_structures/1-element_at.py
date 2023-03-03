02273f92825893863850ef989938283454efdb98
Bn-Bushra <aybuhabi@gmail.com>
2023-03-03 15:33:53 +0100

    another sytle

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif (my_list) > 0:
-        if idx > len(my_list):
-            return None
-        else:
-            return my_list[idx]
+    elif idx > len(my_list):
+        return None
+    else:
+        return my_list[idx]
+        
