4a038143a87b79a473d77531bff08d5d51668d91
Bn-Bushra <aybuhabi@gmail.com>
2023-03-03 23:55:22 +0100

    committed to seat of learning

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0:
-        return my_list
-    elif IndexError:
+    if idx > 0:
+        del my_list[idx]
         return my_list
     else:
-        del my_list[idx]
         return my_list
