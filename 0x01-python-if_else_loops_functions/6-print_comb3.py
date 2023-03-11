f6b782c7f3500977580559b0f509dafffddf61c7
Bn-Bushra <aybuhabi@gmail.com>
2023-03-11 04:18:09 +0100

    pending

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for x in range(10):
     for y in range(10):
         if x != y:
-            # print(f"{x}{y}", end=", ")
             if x and y == y and x:
-                print(f"{x}{y}", end=", ")
+                break
+            print(f"{x}{y}", end=", ")
