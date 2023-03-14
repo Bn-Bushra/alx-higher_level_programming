11fba4f0d4ef0cccc9c49028465b0a16c042d79e
Bn-Bushra <aybuhabi@gmail.com>
2023-03-14 05:15:05 +0100

    i have a lot of value

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # for row in matrix:
-    #     print(row)
-    #     for col in row:
-    #         # print("{}".format(col))
-    #         pass
+    for row in matrix:
+        # print(row)
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "\n")
     # mat = len(matrix)
     # for i in range(mat):
     #     for j in range(mat):
