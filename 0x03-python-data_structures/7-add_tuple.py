0893f294f2ee5de02e06b42fcc280c61b8a0752c
Bn-Bushra <aybuhabi@gmail.com>
2023-03-14 05:02:45 +0100

    at least

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # if tuples contain less than two value, use 1 for the missing one solve?
-    a, b = tuple_a + (0)
-    c, d = tuple_b, (0)
-    i = a + c
-    t = b + d
-    return i, t
+    # if tuples contain less than two value, replace it with zero?
+    t1 = tuple_a + (0, 0)
+    t2 = tuple_b + (0, 0)
+    i = t1[0] + t2[0]
+    j = t1[1] + t2[1]
+    return i, j
