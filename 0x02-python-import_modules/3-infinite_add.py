9cccf8d143ead8280985a58dd8fea354a0d06475
Bn-Bushra <aybuhabi@gmail.com>
2023-03-16 07:30:57 +0100

    pycodestyling

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
-loopLen = len(argv) - 1
-sum = 0
-for a in range(loopLen):
-    arguments = int(argv[a + 1])
-    sum += arguments 
-print(sum)
+if __name__ == "__main__":
+    from sys import argv
+    loopLen = len(argv) - 1
+    sum = 0
+    for a in range(loopLen):
+        arguments = int(argv[a + 1])
+        sum += arguments 
+    print(sum)
